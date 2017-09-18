079c88369158c624e23b1f514bae8790254c11cc
bearics <bearics@gmail.com>
2017-09-18 08:18:19 -0700

    Fin hw

--- week3/hw/2014726001.c
+++ week3/hw/2014726001.c
@@ -10,45 +10,30 @@ int main(int argc, char** argv) {
 	struct stat filestat;
 	int nread=0;
 	char *buf;
-	int size = 0;
 
-	if(fd1 = open(argv[1], O_WRONLY, 0644) > 0) {
-		stat("./testimg", &filestat);
-		printf("stat ok %d ", (int)filestat.st_size);
+	if((fd1 = open(argv[1], O_RDONLY)) > 0) {
+
+		if(stat(argv[1], &filestat) == -1)
+		{
+			perror("fopen");	
+		}
 		buf = (char*)malloc((int)filestat.st_size);
+		memset(buf, 0x00, filestat.st_size);
 		nread = read(fd1, buf, (int)filestat.st_size);
-		printf("%d", nread);
-		/*
-		if(fd2 = open(argv[2], O_WRONLY | O_CREAT | O_EXCL, 0644) > 0) {
+		puts("복사 완료");
+		if( (fd2 = open(argv[2], O_WRONLY | O_CREAT, 0644)) > 0) {
 			write(fd2, buf, nread);
 			close(fd2);
 			close(fd1);
 		}else {
 			close(fd2);
 			close(fd1);
-			printf("저장하는데 실패했습니다.");
-		}*/
-	} else {
-		printf("파일 열기에 실패했습니다.");
-	}
-	/*
-	if((fs1 = fopen("image/test.jpg", "rb")) != NULL ) {
-		puts("dddd");
-		unsigned char *pImgFile;
-
-		fstat(fileno(fs1), &filestat );
-		pImgFile = (unsigned char*)malloc(filestat.st_size);
-		memset(pImgFile, 0x00, filestat.st_size);
-		nread=read((unsigned char*)pImgFile, 1, filestat.st_size, fs1);
-		fclose(fs1);
-
-		if((fs2 = fopen("TestImgData.lsf", "wb")) != NULL ) {
-			nRet = fwrite((void*)pImgFile, filestat.st_size, 1, fs2);
-			fclose(fs2);
+			printf("저장하는데 실패했습니다.\n");
 		}
+	} else {
+		close(fd1);
+		printf("파일 열기에 실패했습니다.\n");
+	}
 
-		free(pImgFile);
-	} // end if
-	*/
 	return 0;
 }
